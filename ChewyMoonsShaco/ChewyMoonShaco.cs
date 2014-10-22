d7eab3c1dafe3f9b27ad86d5fa90dd3976ff4e76
Justin Perez <chewymoonleaguesharp@gmail.com>
2014-10-22 12:10:40 -0400

    update

--- ChewyMoonsShaco/ChewyMoonShaco.cs
+++ ChewyMoonsShaco/ChewyMoonShaco.cs
@@ -21,11 +21,11 @@ namespace ChewyMoonsShaco
 
         private static void GameOnOnGameUpdate(EventArgs args)
         {
+            Console.Clear();
             foreach (var enemy in ObjectManager.Get<Obj_AI_Hero>().Where(enemy => enemy.IsEnemy))
             {
                 Console.WriteLine("{0}: {1}", enemy.BaseSkinName, enemy.Orientation);
             }
-            Console.Clear();
         }
     }
 }
