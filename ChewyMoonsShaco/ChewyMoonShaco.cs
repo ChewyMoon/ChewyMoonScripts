e2202490906ed70a316f66f7a85a9d2484f961a4
Justin Perez <chewymoonleaguesharp@gmail.com>
2014-10-23 21:16:03 -0400

    Update! :^)

--- ChewyMoonsShaco/ChewyMoonShaco.cs
+++ ChewyMoonsShaco/ChewyMoonShaco.cs
@@ -2,8 +2,9 @@
 
 using LeagueSharp;
 using LeagueSharp.Common;
+using LX_Orbwalker;
 using System;
-using System.Linq;
+using System.Drawing;
 
 #endregion
 
@@ -12,21 +13,94 @@ namespace ChewyMoonsShaco
     internal class ChewyMoonShaco
     {
         public static Spell Q;
+        public static Spell W;
+        public static Spell E;
+
+        public static Menu Menu;
+        public static LXOrbwalker Orbwalker;
 
         public static void OnGameLoad(EventArgs args)
         {
             if (ObjectManager.Player.BaseSkinName != "Shaco") return;
 
+            Q = new Spell(SpellSlot.Q, ObjectManager.Player.Spellbook.GetSpell(SpellSlot.Q).SData.CastRadius[0]);
+            W = new Spell(SpellSlot.W, ObjectManager.Player.Spellbook.GetSpell(SpellSlot.W).SData.CastRadius[0]);
+            E = new Spell(SpellSlot.E, ObjectManager.Player.Spellbook.GetSpell(SpellSlot.E).SData.CastRadius[0]);
+
+            CreateMenu();
+
             Game.OnGameUpdate += GameOnOnGameUpdate;
+            Drawing.OnDraw += Drawing_OnDraw;
+        }
+
+        private static void CreateMenu()
+        {
+            (Menu = new Menu("[Chewy's Shaco]", "cmShaco", true)).AddToMainMenu();
+
+            // Target Selector
+            var tsMenu = new Menu("Target Selector", "cmShacoTS");
+            SimpleTs.AddToMenu(tsMenu);
+            Menu.AddSubMenu(tsMenu);
+
+            // Orbwalking
+            var orbwalkingMenu = new Menu("Orbwalking", "cmShacoOrbwalkin");
+            Orbwalker = new LXOrbwalker();
+            LXOrbwalker.AddToMenu(orbwalkingMenu);
+            Menu.AddSubMenu(orbwalkingMenu);
+
+            // Combo
+            var comboMenu = new Menu("Combo", "cmShacoCombo");
+            comboMenu.AddItem(new MenuItem("useQ", "Use Q").SetValue(true));
+            comboMenu.AddItem(new MenuItem("useW", "Use W").SetValue(true));
+            comboMenu.AddItem(new MenuItem("useE", "Use E").SetValue(true));
+            Menu.AddSubMenu(comboMenu);
+
+            // Harass
+            var harassMenu = new Menu("Harass", "cmShacoHarass");
+            harassMenu.AddItem(new MenuItem("useEHarass", "Use E").SetValue(true));
+            Menu.AddSubMenu(harassMenu);
+
+            // Drawing
+            var drawingMenu = new Menu("Drawings", "cmShacoDrawing");
+            drawingMenu.AddItem(new MenuItem("drawQ", "Draw Q").SetValue(new Circle(true, Color.Khaki)));
+            drawingMenu.AddItem(new MenuItem("drawW", "Draw W").SetValue(new Circle(true, Color.Khaki)));
+            drawingMenu.AddItem(new MenuItem("drawE", "Draw E").SetValue(new Circle(true, Color.Khaki)));
+            Menu.AddSubMenu(drawingMenu);
+
+            // Misc
+            var miscMenu = new Menu("Misc", "cmShacoMisc");
+            miscMenu.AddItem(new MenuItem("stuff", "Let me know of any"));
+            miscMenu.AddItem(new MenuItem("stuff2", "other misc features you want"));
+            miscMenu.AddItem(new MenuItem("stuff3", "on the thead or IRC"));
+            Menu.AddSubMenu(miscMenu);
+        }
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            var qCircle = Menu.Item("drawQ").GetValue<Circle>();
+            var wCircle = Menu.Item("drawW").GetValue<Circle>();
+            var eCircle = Menu.Item("drawE").GetValue<Circle>();
+
+            var pos = ObjectManager.Player.Position;
+
+            if (qCircle.Active)
+            {
+                Utility.DrawCircle(pos, Q.Range, qCircle.Color);
+            }
+
+            if (wCircle.Active)
+            {
+                Utility.DrawCircle(pos, W.Range, wCircle.Color);
+            }
+
+            if (eCircle.Active)
+            {
+                Utility.DrawCircle(pos, E.Range, eCircle.Color);
+            }
         }
 
         private static void GameOnOnGameUpdate(EventArgs args)
         {
-            Console.Clear();
-            foreach (var enemy in ObjectManager.Get<Obj_AI_Base>().Where(enemy => enemy.IsEnemy))
-            {
-                Console.WriteLine("{0}: {1}", enemy.BaseSkinName, enemy.Orientation);
-            }
         }
     }
 }
