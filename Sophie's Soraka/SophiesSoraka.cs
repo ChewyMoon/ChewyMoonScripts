a00ddf39dd335fafd03c9634e631f5cdfce5fb89
Sophie <aprilsophiemay@gmail.com>
2014-11-12 15:29:14 +1100

    Update SophiesSoraka.cs

--- Sophie's Soraka/SophiesSoraka.cs
+++ Sophie's Soraka/SophiesSoraka.cs
@@ -186,7 +186,12 @@ namespace Sophies_Soraka
             comboMenu.AddItem(new MenuItem("useQ", "Use Q").SetValue(true));
             comboMenu.AddItem(new MenuItem("useE", "Use E").SetValue(true));
             Menu.AddSubMenu(comboMenu);
-
+           
+            // Exploit
+            var harassMenu = new Menu("Exploit", "ssExploit");
+            exploitMenu.AddItem(new MenuItem("exploit", "Exploit").SetValue(true));
+            Menu.AddSubMenu(harassMenu);
+            
             // Misc
             var miscMenu = new Menu("Misc", "ssMisc");
             miscMenu.AddItem(new MenuItem("packets", "Use Packets").SetValue(true));
