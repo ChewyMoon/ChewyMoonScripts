10e03e4bcba660234f1a154902a96da1c4e2103f
Justin Perez <chewymoonleaguesharp@gmail.com>
2014-11-24 21:47:00 -0500

    This should fix it?? idk

--- Golderino/Program.cs
+++ Golderino/Program.cs
@@ -1,21 +1,17 @@
-﻿using System;
-using System.Resources;
-using System.Collections.Generic;
+﻿#region
+
+using System;
 using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
-using SharpDX.Direct3D9;
+
+#endregion
 
 namespace Golderino
 {
-    class Program
+    internal class Program
     {
-
         private static float _myTeamGold;
         private static float _enemyTeamGold;
         private static float _goldAdvantage;
@@ -23,88 +19,80 @@ namespace Golderino
         private static Render.Sprite _greenBar;
         private static Render.Sprite _redBar;
 
-        private static Render.Text leftText;
-        private static Render.Text middleText;
-        private static Render.Text rightText;
-
+        private static Render.Text _leftText;
+        private static Render.Text _middleText;
+        private static Render.Text _rightText;
 
         public static int ImgWidth = 437;
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             CustomEvents.Game.OnGameLoad += GameOnOnGameLoad;
         }
 
         private static void GameOnOnGameLoad(EventArgs args)
         {
-            _greenBar = new Render.Sprite(Resource1.bar_green, new Vector2(Drawing.Width / 2 - ImgWidth / 2, 100));
-            _redBar = new Render.Sprite(Resource1.bar_red, new Vector2(Drawing.Width / 2 - ImgWidth / 2, 100));
+            _greenBar = new Render.Sprite(Resource1.bar_green, new Vector2(Drawing.Width/2 - ImgWidth/2, 100));
+            _redBar = new Render.Sprite(Resource1.bar_red, new Vector2(Drawing.Width/2 - ImgWidth/2, 100));
 
-            leftText = new Render.Text("2375", _redBar.X - Drawing.GetTextExtent("2375").Width, _redBar.Y, 12, Color.White);
-            middleText = new Render.Text("50%", Drawing.Width/2 - Drawing.GetTextExtent("50%").Width/2, _redBar.Y ,12, Color.White);
-            rightText = new Render.Text("2375", _redBar.X + Drawing.GetTextExtent("2375").Width, _redBar.Y, 12, Color.White);
+            _leftText = new Render.Text("2375", _redBar.X - Drawing.GetTextExtent("2375").Width, _redBar.Y, 12,
+                Color.White);
+            _middleText = new Render.Text("50%", Drawing.Width/2 - Drawing.GetTextExtent("50%").Width/2, _redBar.Y, 12,
+                Color.White);
+            _rightText = new Render.Text("2375", _redBar.X + Drawing.GetTextExtent("2375").Width, _redBar.Y, 12,
+                Color.White);
 
             _redBar.Add();
             _greenBar.Add();
-            leftText.Add();
-            middleText.Add();
-            rightText.Add();
+            _leftText.Add();
+            _middleText.Add();
+            _rightText.Add();
 
-            Drawing.OnEndScene += delegate
-            { _redBar.OnEndScene(); _greenBar.OnEndScene(); };
-
-            Drawing.OnPostReset += delegate
-            { _redBar.OnPostReset(); _greenBar.OnPostReset(); };
-
-            Drawing.OnPreReset += delegate
-            { _redBar.OnPreReset(); _greenBar.OnPreReset(); };
-
-            UpdateGold();
+            Game.OnGameProcessPacket += Game_OnGameProcessPacket;
+            Game.OnGameUpdate += GameOnOnGameUpdate;
 
             Game.PrintChat("Golderino by ChewyMoon loaded.");
         }
 
-        private static void UpdateGold()
+        private static void GameOnOnGameUpdate(EventArgs args)
         {
             _greenBar.Reset();
-            ResetVariables();
-            Console.Clear();
 
-            foreach (var friend in ObjectManager.Get<Obj_AI_Hero>().Where(x => x.IsAlly))
-            {
-                _myTeamGold += friend.GoldEarned;
-                Console.WriteLine(@"{0}: {1}", friend.ChampionName, friend.GoldEarned);
-            }
+            _goldAdvantage = (float) Math.Round(_myTeamGold/(_myTeamGold + _enemyTeamGold)*100, 1);
 
-            foreach (var enemy in ObjectManager.Get<Obj_AI_Hero>().Where(x => x.IsEnemy))
-            {
-                _enemyTeamGold += enemy.GoldEarned;
-                Console.WriteLine(@"{0}: {1}", enemy.ChampionName, enemy.GoldEarned);
-            }
+            var width = (_goldAdvantage/100)*ImgWidth;
+            _greenBar.Crop(new Rectangle(_greenBar.X, _greenBar.Y, (int) width, _greenBar.Height), true);
 
-            var total = _myTeamGold + _enemyTeamGold;
-            _goldAdvantage = (float)Math.Round(_myTeamGold / total * 100, 1);
+            _leftText.text = _myTeamGold.ToString(CultureInfo.InvariantCulture) + "g";
+            _leftText.X = _redBar.X - Drawing.GetTextExtent(_leftText.text).Width;
 
-            var width = (_goldAdvantage / 100) * ImgWidth;
-            _greenBar.Crop(new Rectangle(_greenBar.X, _greenBar.Y, (int)width, _greenBar.Height), true);
+            _middleText.text = _goldAdvantage + "%";
+            _middleText.X = Drawing.Width/2 -
+                           Drawing.GetTextExtent(_goldAdvantage.ToString(CultureInfo.InvariantCulture)).Width/2;
 
-            leftText.text = _myTeamGold.ToString(CultureInfo.InvariantCulture) + "g";
-            leftText.X = _redBar.X - Drawing.GetTextExtent(leftText.text).Width;
-
-            middleText.text = _goldAdvantage + "%";
-            middleText.X = Drawing.Width/2 - Drawing.GetTextExtent(_goldAdvantage.ToString(CultureInfo.InvariantCulture)).Width/2;
-
-            rightText.text = _enemyTeamGold.ToString(CultureInfo.InvariantCulture) + "g";
-            rightText.X = _redBar.X + Drawing.GetTextExtent(rightText.text).Width;
-
-            Utility.DelayAction.Add(1000, UpdateGold);
+            _rightText.text = _enemyTeamGold.ToString(CultureInfo.InvariantCulture) + "g";
+            _rightText.X = _redBar.X + Drawing.GetTextExtent(_rightText.text).Width;
         }
 
-        private static void ResetVariables()
+        private static void Game_OnGameProcessPacket(GamePacketEventArgs args)
         {
-            _myTeamGold = 0;
-            _enemyTeamGold = 0;
-            _goldAdvantage = 0;
+            var data = args.PacketData;
+            if (data[0] != Packet.S2C.AddGold.Header)
+                return;
+
+            var decoded = Packet.S2C.AddGold.Decoded(data);
+            if (decoded.ReceivingUnit.IsEnemy)
+            {
+                _enemyTeamGold += decoded.Gold;
+            }
+            else if (decoded.ReceivingUnit.IsAlly)
+            {
+                _myTeamGold += decoded.Gold;
+            }
+            else
+            {
+                Console.WriteLine(@"Uhh, Chewy? I think you dun goofed.");
+            }
         }
     }
 }
