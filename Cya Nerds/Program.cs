0cacaf998523139fcc5fad5e2878ffdd69b353df
Justin Perez <chewymoonleaguesharp@gmail.com>
2014-11-25 18:16:47 -0500

    Release xD

--- Cya Nerds/Program.cs
+++ Cya Nerds/Program.cs
@@ -1,29 +1,31 @@
-﻿using System;
+﻿#region
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using LeagueSharp;
 using LeagueSharp.Common;
 
+#endregion
+
 namespace Cya_Nerds
 {
-    struct WardJumpChampion
+    internal struct WardJumpChampion
     {
-        public Spell WardJumpSpell;
         public string ChampionName;
+        public Spell WardJumpSpell;
 
         public WardJumpChampion(string champName, Spell wardJumpSpell)
         {
             ChampionName = champName;
-            WardJumpSpell = wardJumpSpell;           
+            WardJumpSpell = wardJumpSpell;
         }
     }
 
-    class Program
+    internal class Program
     {
         public static List<WardJumpChampion> WardJumpChampions;
-        private static WardJumpChampion plugin;
+        private static WardJumpChampion _plugin;
         private static int _lastWardPlacedT;
         private static Menu _menu;
 
@@ -32,7 +34,7 @@ namespace Cya_Nerds
             get { return _menu.Item("wardJump").GetValue<KeyBind>().Active; }
         }
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             WardJumpChampions = new List<WardJumpChampion>
             {
@@ -44,12 +46,13 @@ namespace Cya_Nerds
             CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
         }
 
-        static void Game_OnGameLoad(EventArgs args)
+        private static void Game_OnGameLoad(EventArgs args)
         {
             var loaded = false;
-            foreach (var champ in WardJumpChampions.Where(champ => champ.ChampionName == ObjectManager.Player.ChampionName))
+            foreach (
+                var champ in WardJumpChampions.Where(champ => champ.ChampionName == ObjectManager.Player.ChampionName))
             {
-                plugin = champ;
+                _plugin = champ;
                 loaded = true;
                 break;
             }
@@ -63,11 +66,10 @@ namespace Cya_Nerds
             _menu.AddItem(new MenuItem("wardDelay", "Ward Delay(MS)").SetValue(new Slider(3000, 0, 10*1000)));
             _menu.AddItem(new MenuItem("wardJump", "Ward Jump").SetValue(new KeyBind('t', KeyBindType.Press)));
             _menu.AddToMainMenu();
-      
+
             GameObject.OnCreate += GameObjectOnOnCreate;
             Game.OnGameUpdate += GameOnOnGameUpdate;
             Game.PrintChat("Cya Nerds by ChewyMoon loaded.");
-
         }
 
         private static void GameObjectOnOnCreate(GameObject sender, EventArgs args)
@@ -82,16 +84,15 @@ namespace Cya_Nerds
                 return;
 
             var ward = (Obj_AI_Minion) sender;
-            if(sender.Position.Distance(ObjectManager.Player.ServerPosition) <= plugin.WardJumpSpell.Range)
-                plugin.WardJumpSpell.CastOnUnit(ward);
-
+            if (sender.Position.Distance(ObjectManager.Player.ServerPosition) <= _plugin.WardJumpSpell.Range)
+                _plugin.WardJumpSpell.CastOnUnit(ward);
         }
 
         private static void GameOnOnGameUpdate(EventArgs args)
         {
             // Jump to Minion, Hero & ward
 
-            if(!plugin.WardJumpSpell.IsReady() || !WardJump)
+            if (!_plugin.WardJumpSpell.IsReady() || !WardJump)
                 return;
 
             var jumpRange = _menu.Item("jumpRange").GetValue<Slider>().Value;
@@ -102,27 +103,32 @@ namespace Cya_Nerds
                         .Where(x => x.Name.ToUpper().Contains("WARD"))
                         .Where(x => x.Distance(Game.CursorPos) < jumpRange))
             {
-                plugin.WardJumpSpell.CastOnUnit(ward);
+                _plugin.WardJumpSpell.CastOnUnit(ward);
                 return;
             }
 
             foreach (
                 var hero in
-                    ObjectManager.Get<Obj_AI_Hero>().Where(x => !x.IsDead).Where(x => x.Distance(Game.CursorPos) < jumpRange))
+                    ObjectManager.Get<Obj_AI_Hero>()
+                        .Where(x => !x.IsDead)
+                        .Where(x => x.Distance(Game.CursorPos) < jumpRange))
             {
-                plugin.WardJumpSpell.CastOnUnit(hero);
+                _plugin.WardJumpSpell.CastOnUnit(hero);
                 return;
             }
 
-            foreach (var minion in ObjectManager.Get<Obj_AI_Minion>().Where(x => x.Distance(Game.CursorPos) < jumpRange))
+            foreach (var minion in ObjectManager.Get<Obj_AI_Minion>().Where(x => x.Distance(Game.CursorPos) < jumpRange)
+                )
             {
-                plugin.WardJumpSpell.CastOnUnit(minion);
+                _plugin.WardJumpSpell.CastOnUnit(minion);
                 return;
             }
 
             //now we have to place ward :<
-            if (Environment.TickCount < _lastWardPlacedT + _menu.Item("wardDelay").GetValue<Slider>().Value) return;
-            if (!plugin.WardJumpSpell.IsReady())
+            if (Environment.TickCount < _lastWardPlacedT + _menu.Item("wardDelay").GetValue<Slider>().Value)
+                return;
+
+            if (!_plugin.WardJumpSpell.IsReady())
                 return;
 
             var wardSlot = Items.GetWardSlot();
