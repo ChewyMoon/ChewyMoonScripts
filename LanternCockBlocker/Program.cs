5e53e355d41954fa321732025de725ede35bceea
Justin Perez <chewymoonleaguesharp@gmail.com>
2015-01-03 13:51:51 -0500

    Final update

--- LanternCockBlocker/Program.cs
+++ LanternCockBlocker/Program.cs
@@ -1,47 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿#region
+
+using System;
 using LeagueSharp;
 using LeagueSharp.Common;
 
+#endregion
+
 namespace LanternCockBlocker
 {
-    class Program
+    internal class Program
     {
-        private static Menu menu;
+        private static Menu _menu;
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             CustomEvents.Game.OnGameLoad += GameOnOnGameLoad;
         }
 
         private static void GameOnOnGameLoad(EventArgs args)
         {
-            menu = new Menu("Lantern Blocker", "cmLCB");
-            menu.AddItem(new MenuItem("enabled", "Enabled").SetValue(new KeyBind(32, KeyBindType.Press)));
-            menu.AddToMainMenu();
+            _menu = new Menu("Lantern Blocker", "cmLCB");
+            _menu.AddItem(new MenuItem("enabled", "Enabled").SetValue(new KeyBind(32, KeyBindType.Press)));
+            _menu.AddItem(new MenuItem("delay", "Delay(MS)").SetValue(new Slider(150, 0, 2000)));
+            _menu.AddToMainMenu();
 
             Game.PrintChat("Lantern Cock Blocker by ChewyMoon loaded!");
 
             GameObject.OnCreate += GameObject_OnCreate;
         }
 
-        static void GameObject_OnCreate(GameObject sender, EventArgs args)
+        private static void GameObject_OnCreate(GameObject sender, EventArgs args)
         {
-            if (sender.Name != "ThreshLantern" || sender.IsAlly || !menu.Item("enabled").GetValue<bool>())
+            if (sender.Name != "ThreshLantern" || sender.IsAlly || !_menu.Item("enabled").GetValue<bool>())
                 return;
 
             if (sender.Position.Distance(ObjectManager.Player.ServerPosition) > 600)
                 return;
 
-            var ward = Items.GetWardSlot();
-
-            if (Items.CanUseItem((int)ward.Id))
+            Utility.DelayAction.Add(_menu.Item("delay").GetValue<Slider>().Value, delegate
             {
-                Items.UseItem((int) ward.Id, sender.Position);
-            }
+                var ward = Items.GetWardSlot();
+
+                if (Items.CanUseItem((int) ward.Id))
+                {
+                    Items.UseItem((int) ward.Id, sender.Position);
+                }
+            });
         }
     }
 }
