0db1ff33b7a4534fec765f3a4c9505e34a87bad6
Justin Perez <chewymoonleaguesharp@gmail.com>
2015-01-02 21:02:57 -0500

    Added auto ward on enemy thresh lantern

---
+++ LanternCockBlocker/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace LanternCockBlocker
+{
+    class Program
+    {
+        private static Menu menu;
+
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += GameOnOnGameLoad;
+        }
+
+        private static void GameOnOnGameLoad(EventArgs args)
+        {
+            menu = new Menu("Lantern Cock Blocker", "cmLCB");
+            menu.AddItem(new MenuItem("enabled", "Enabled").SetValue(true));
+            menu.AddToMainMenu();
+
+            Game.PrintChat("Lantern Cock Blocker by ChewyMoon loaded!");
+
+            GameObject.OnCreate += GameObject_OnCreate;
+        }
+
+        static void GameObject_OnCreate(GameObject sender, EventArgs args)
+        {
+            if (sender.Name != "ThreshLantern" || sender.IsAlly || !menu.Item("enabled").GetValue<bool>())
+                return;
+
+            if (sender.Position.Distance(ObjectManager.Player.ServerPosition) > 600)
+                return;
+
+            var ward = Items.GetWardSlot();
+
+            if (Items.CanUseItem((int)ward.Id))
+            {
+                Items.UseItem((int) ward.Id, sender.Position);
+            }
+        }
+    }
+}
