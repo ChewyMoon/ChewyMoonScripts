d355d3e085aa3f2837fd4849c6b2c4a8b6b33fea
Justin <chewymoonleaguesharp@gmail.com>
2015-09-04 13:14:17 -0400

    Uploaded Legacy Soraka

---
+++ Sophie's Soraka/Program.cs
@@ -0,0 +1,16 @@
+﻿#region
+
+using LeagueSharp.Common;
+
+#endregion
+
+namespace Sophies_Soraka
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += SophiesSoraka.OnGameLoad;
+        }
+    }
+}
