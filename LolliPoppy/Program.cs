c3123443a34ec328fe61879ce7fb6587a5d110c3
Justin Perez <chewymoonleaguesharp@gmail.com>
2016-01-05 22:35:26 -0500

    upload lollipoppy

---
+++ LolliPoppy/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace LolliPoppy
+{
+    using LeagueSharp.Common;
+
+    internal class Program
+    {
+        #region Methods
+
+        /// <summary>
+        /// The entry point of the application
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(System.EventArgs args)
+        {
+            
+        }
+
+        #endregion
+    }
+}
--- TrapEvade/Properties/AssemblyInfo.cs
+++ TrapEvade/Properties/AssemblyInfo.cs
@@ -1,32 +1,26 @@
-﻿#region
-
-using System.Reflection;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-#endregion
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("Mid or Feed")]
+[assembly: AssemblyTitle("TrapEvade")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mid or Feed")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("TrapEvade")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("a285d029-9a69-4311-9d98-54e321c05459")]
+[assembly: Guid("1362427d-4344-4127-8230-f93636e14b15")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -38,6 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.5.24.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
